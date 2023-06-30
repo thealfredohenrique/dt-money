370e209c2b984a49099b708a34941f095428d194
Alfredo Henrique <thealfredohenrique@gmail.com>
2023-06-29 22:08:10 -0300

    feat(summary): Add summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,38 @@
+import {
+  ArrowCircleDown,
+  ArrowCircleUp,
+  CurrencyDollar,
+} from "@phosphor-icons/react";
+import { SummaryCard, SummaryContainer } from "./styles";
+
+function Summary() {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00B37E" />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#F75A68" />
+        </header>
+        <strong>R$ 1.259,00</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Entradas</span>
+          <CurrencyDollar size={32} color="#FFFFFF" />
+        </header>
+        <strong>R$ 16.141,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  );
+}
+
+export default Summary;
